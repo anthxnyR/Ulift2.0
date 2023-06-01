638e1662ccec6f534ddcec1b4cd71d7a71606528
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-06-01 13:46:07 -0400

    Arreglando CORS

--- Ulift2.0/Program.cs
+++ Ulift2.0/Program.cs
@@ -18,6 +18,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
 app.UseAuthorization();
 
 app.MapControllers();

90814459e37cee5a0d1377ca581d8a6de72536dd
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-06-03 21:10:46 -0400

    testing

--- Ulift2.0/Program.cs
+++ Ulift2.0/Program.cs
@@ -7,49 +7,20 @@ builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-<<<<<<< HEAD
-=======
-
-
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000");
-        });
-});
->>>>>>> master
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
-<<<<<<< HEAD
 app.UseCors();
-=======
-app.UseCors(MyAllowSpecificOrigins);
->>>>>>> master
-
 app.UseAuthorization();
-
 app.UseAuthentication();
 
 app.MapControllers();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
 
 app.Run();

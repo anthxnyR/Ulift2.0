c6bc6c6cbf0450ba91ee2c878dcdb58b5ca275aa
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-06-13 02:16:02 -0400

    que

--- Ulift2.0/Repository/AuthCollection.cs
+++ Ulift2.0/Repository/AuthCollection.cs
@@ -166,7 +166,7 @@ namespace Ulift2._0.Repository
             var url = "https://ulift.azurewebsites.net/api/Auth/" + "Verify?token=" + tokenMailVerification;
             Console.WriteLine(url);
 
-            string htmlFilePath = Path.Combine(AppDomain.CurrentDomain, "Assets", "EmailConfirmation.html");
+            string htmlFilePath = @"Ulift2.0/Assets/EmailConfirmation.html";
             string htmlBody = System.IO.File.ReadAllText(htmlFilePath);
 
             var mailMessage = new MailMessage

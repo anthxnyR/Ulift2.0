9b4fb0cd09eb37da0074c75651e4ddb88b9aca09
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-06-14 21:14:49 -0400

    Validaciones modulo Favoritos

--- Ulift2.0/Repository/FavoriteCollection.cs
+++ Ulift2.0/Repository/FavoriteCollection.cs
@@ -33,16 +33,38 @@ namespace Ulift2._0.Repository
 
         public void ValidateFavoriteAttributes(Favorite favorite, ModelStateDictionary ModelState)
         {
+            if (favorite.UserEmail == favorite.FavoriteEmail)
+            {
+                ModelState.AddModelError("FavoriteEmail", "El usuario no puede ser su propio favorito");
+                throw new Exception("El usuario no puede ser su propio favorito");
+            }
+
+            IMongoDatabase mongoDatabase = _repository.db;
+            IMongoCollection<User> userCollection = mongoDatabase.GetCollection<User>("Users");
+
+            var filter = Builders<User>.Filter.Eq("Email", favorite.UserEmail);
+            if (userCollection.Find(filter).FirstOrDefault() == null)
+            {
+                ModelState.AddModelError("UserEmail", "El usuario no existe");
+                throw new Exception("El usuario principal no existe");
+            }
+            filter = Builders<User>.Filter.Eq("Email", favorite.FavoriteEmail);
+            if (userCollection.Find(filter).FirstOrDefault() == null)
+            {
+                ModelState.AddModelError("FavoriteEmail", "El usuario favorito no existe");
+                throw new Exception("El usuario que desea agregar como favorito no existe");
+            }
+
             string domainPattern = @"@(est.ucab.edu.ve|ucab.edu.ve)$";
             if (!Regex.IsMatch(favorite.UserEmail, domainPattern, RegexOptions.IgnoreCase))
             {
                 ModelState.AddModelError("Email", "El correo electrónico no pertenece al dominio UCAB");
-                throw new Exception("El correo electrónico no pertenece al dominio UCAB");
+                throw new Exception("El correo electrónico del Usuario no pertenece al dominio UCAB");
             }
             if (!Regex.IsMatch(favorite.FavoriteEmail, domainPattern, RegexOptions.IgnoreCase))
             {
                 ModelState.AddModelError("Email", "El correo electrónico no pertenece al dominio UCAB");
-                throw new Exception("El correo electrónico no pertenece al dominio UCAB");
+                throw new Exception("El correo electrónico del Usuario Favorito no pertenece al dominio UCAB");
             }
             if (favorite.UserEmail == null)
             {

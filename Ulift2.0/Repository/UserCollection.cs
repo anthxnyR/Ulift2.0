9a100f26c9382c7fd5bbcc056d0e1c0a615d0d59
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-06-14 22:16:26 -0400

    Cambio en consulta de rutas

--- Ulift2.0/Repository/UserCollection.cs
+++ Ulift2.0/Repository/UserCollection.cs
@@ -66,7 +66,7 @@ namespace Ulift2._0.Repository
             var user = await Collection.Find(u => u.Email == email).FirstOrDefaultAsync();
             var vehicles = await _repository.db.GetCollection<Vehicle>("Vehicles").Find(v => v.Email == email).ToListAsync();
             var destinations = await _repository.db.GetCollection<Destination>("Destinations").Find(d => d.Email == email).ToListAsync();
-            var routes = await _repository.db.GetCollection<URoute>("URoutes").Find(r => r.Email == email).ToListAsync();
+            var routes = await _repository.db.GetCollection<URoute>("Routes").Find(r => r.Email == email).ToListAsync();
 
             return new {
                 User = user,

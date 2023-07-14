4a11b90935c25b93c5747056131c13d06a737ffc
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-07-14 10:22:40 -0400

    cambios

--- Ulift2.0/Controllers/AuthController.cs
+++ Ulift2.0/Controllers/AuthController.cs
@@ -76,7 +76,7 @@ namespace Ulift2._0.Controllers
             else
             {
                 await db.Verify(token);
-                return Redirect("https://--main.u-lift.netlify.app");
+                return Redirect("https://u-lift.netlify.app");
             }
         }
     }

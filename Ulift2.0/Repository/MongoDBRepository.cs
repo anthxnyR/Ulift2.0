4a2103f1d21ceb1d492d2ffd66d2b812be9bded2
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2023-07-24 10:04:15 -0400

    cambio de db

--- Ulift2.0/Repository/MongoDBRepository.cs
+++ Ulift2.0/Repository/MongoDBRepository.cs
@@ -12,7 +12,7 @@ namespace Ulift2._0.Repository
         public IMongoDatabase db;
         public MongoDBRepository()
         {
-            client = new MongoClient("mongodb://mongo:pJEPTT2uLbKRGuWMGMKT@containers-us-west-179.railway.app:6721");
+            client = new MongoClient("mongodb://mongo:79oqX31rEX2kLUCTjJvd@containers-us-west-146.railway.app:7770");
             db = client.GetDatabase("Ulift");
         }
     }
